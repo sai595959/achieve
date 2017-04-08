8344c20912a23d83900a8f35639d469fdc794457
John Doe <johndoe@example.com>
2017-04-08 14:21:21 +0000

    make it better

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
+# -*- encoding: utf-8 -*-
+
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  
+
     # before_actionで下で定義したメソッドを実行
   before_action :configure_permitted_parameters, if: :devise_controller?
 
