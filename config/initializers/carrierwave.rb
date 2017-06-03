6c8a475278a4812ad4dcd3939d91bea6a5d1b59f
simon <sai590817@gmail.com>
2017-06-03 20:39:44 +0900

    init unicorn

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,10 +12,10 @@ CarrierWave.configure do |config|
 
   case Rails.env
     when 'production'
-      config.fog_directory = '本番環境用のバケット名'
+      config.fog_directory = 'achieve-production-gokyu'
       config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/achieve-production-gokyu'
     when 'development'
-      config.fog_directory = '開発環境用のバケット名'
+      config.fog_directory = 'achieve-development-gokyu'
       config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/achieve-development-gokyu'
   end
 end
